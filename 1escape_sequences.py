ad49587124f22980eb7435985ab111427076fc40
abvk20 <ab.vk20@gmail.com>
2021-01-15 08:50:49 +0530

    First commit of this project and also my life

---
+++ 1escape_sequences.py
@@ -0,0 +1,10 @@
+print("Groww", end="%^&") #By default end=\n
+print("Hello\b World!")  #Backspace
+print("\a") # ASCII BEL
+print("Hello \f World!")  # ASCII FormFeed
+print("Hello \n World!")  # ASCII LineFeed
+print("Hellofkdsaldlkjfkldsjfdklsjfakljsdfkjdfkllaskjf;adj;lfjd;a;l \r World!")  # ASCII Carriage Return
+print("ASCII \t Horizontal Tab")
+print("ASCII \v Vertical Tab")
+print("\110\145\154\154\157\40\127\157\162\154\144\41")  # Character with octal values
+print("\x48\x65\x6c\x6c\x6f\x20\x57\x6f\x72\x6c\x64\x21")  # Character with hex values
